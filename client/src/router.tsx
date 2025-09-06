053193cd6274856626269f3f843675625ae8fa20
donya <thisis2nya@gmail.com>
2025-09-06 19:49:37 +0330

    feat(login): create Login page & form

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,10 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 
 // components
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
