aa393f0e7dd1156bc9f688c94a5f37656ea32c2b
donya <thisis2nya@gmail.com>
2025-09-08 17:29:36 +0330

    feat(user): define new Panel routes & their pages

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,10 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 // components
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AboutUs from "./pages/About";
+import PanelLayout from "./layouts/Dashboard";
+import Dashboard from "./pages/user/Dashboard";
 
 export const router = createBrowserRouter([
   {
@@ -22,5 +24,24 @@ export const router = createBrowserRouter([
   {
     path: "/about-us",
     element: <AboutUs />,
+  },
+
+  {
+    path: "/panel",
+    element: <Navigate to="/panel" replace />,
+  },
+  {
+    path: "/panel",
+    element: <PanelLayout />,
+    children: [
+      {
+        path: "",
+        element: <Dashboard />,
+      },
+      {
+        path: "resume/create",
+        element: <p>Resume Create Page</p>,
+      },
+    ],
   },
 ]);
