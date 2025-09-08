0a90162c88d285627b25d1496b1f6e0333c342d0
donya <thisis2nya@gmail.com>
2025-09-08 17:26:03 +0330

    chore(user): define Resume & Sidebar types

---
+++ client/src/types/resume.ts
@@ -0,0 +1,7 @@
+export interface Resume {
+  id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  isCompleted: boolean;
+}
---
+++ client/src/types/user.ts
@@ -0,0 +1,6 @@
+export interface SidebarItem {
+  id: string;
+  label: string;
+  path: string;
+  icon?: React.ReactNode;
+}
