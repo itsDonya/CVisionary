960fc3e0ae540d172602bc6f56adf2eadd947e05
donya <thisis2nya@gmail.com>
2025-09-14 19:28:52 +0330

    fix(layout): hide default Header for Home page

--- src/shared/layouts/Default.tsx
+++ src/shared/layouts/Default.tsx
@@ -1,20 +1,22 @@
-import type { ReactNode } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 // components
 import Header from "../components/layouts/components/default/Header";
 import Footer from "../components/layouts/components/default/Footer";
-import { Outlet } from "react-router-dom";
 
-interface PropTypes {
-  children?: ReactNode;
-  hasHeader?: boolean;
-}
+const DefaultLayout = () => {
+  const location = useLocation();
 
-const DefaultLayout = ({ hasHeader = true }: PropTypes) => {
+  // methods
+  const isHomePage = () => {
+    return location.pathname === "/";
+  };
+
   return (
     <>
       <div className="w-full bg-[url('/src/assets/images/landing-bg.jpg')] bg-cover bg-center bg-fixed flex flex-col items-center justify-start gap-10">
-        {hasHeader && <Header />}
+        {!isHomePage() && <Header />}
+
         <main className="w-full">
           <Outlet />
         </main>
